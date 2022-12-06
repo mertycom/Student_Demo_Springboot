67a703e448aac5e312910a620ff1f99b8a9c984a
Barış Mert Cömertoğlu <mertycom@gmail.com>
2022-12-07 00:20:03 +0300

    DeleteMapping added

--- src/main/java/com/example/studentdemo/controller/StudentController.java
+++ src/main/java/com/example/studentdemo/controller/StudentController.java
@@ -3,9 +3,7 @@ package com.example.studentdemo.controller;
 import com.example.studentdemo.model.Student;
 import com.example.studentdemo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -25,4 +23,15 @@ public class StudentController {
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
 }
--- src/main/java/com/example/studentdemo/service/StudentService.java
+++ src/main/java/com/example/studentdemo/service/StudentService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -23,5 +24,21 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("Student with id "+ studentId+ " does not exists");
+        }
+        studentRepository.deleteById(studentId);
     }
 }
